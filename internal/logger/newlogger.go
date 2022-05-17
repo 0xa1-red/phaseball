0f5cd160419d87e45b930e05193fc5344f95790b
Alfred Dobradi <alfreddobradi@gmail.com>
2022-05-17 15:40:43 +0100

    feat: Log into JSON files

--- internal/logger/newlogger.go
+++ internal/logger/newlogger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"github.com/0xa1-red/phaseball/internal/config"
+	"github.com/0xa1-red/phaseball/internal/logger/json"
 	"github.com/0xa1-red/phaseball/internal/logger/logcore"
 	"github.com/0xa1-red/phaseball/internal/logger/psql"
 	"github.com/0xa1-red/phaseball/internal/logger/stdout"
@@ -11,6 +12,7 @@ import (
 const (
 	KindStdout string = "stdout"
 	KindPsql   string = "psql"
+	KindJSON   string = "json"
 )
 
 func NewGameLogger(id uuid.UUID) logcore.GameLog {
@@ -21,5 +23,7 @@ func NewGameLogger(id uuid.UUID) logcore.GameLog {
 		return stdout.New(logcore.WithTimestamp(), logcore.WithGameID(id))
 	case KindPsql:
 		return psql.New(logcore.WithTimestamp(), logcore.WithGameID(id))
+	case KindJSON:
+		return json.New(logcore.WithTimestamp(), logcore.WithGameID(id))
 	}
 }
